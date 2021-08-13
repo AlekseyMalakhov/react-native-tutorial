5664052f51da89d1b947880e48d92b97fcd73e67
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-08-13 09:09:44 +0300

    add loading animation

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Screen from "../components/Screen";
 import Card from "../components/Card";
-import { StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList, View } from "react-native";
 import colors from "../config/colors";
+import listingsApi from "../api/listings";
+import AppText from "../components/AppText";
+import AppButton from "../components/AppButton";
+import ActivityIndicator from "../components/ActivityIndicator";
 
 const styles = StyleSheet.create({
     container: {
@@ -11,24 +15,37 @@ const styles = StyleSheet.create({
     },
 });
 
-const listings = [
-    {
-        id: 1,
-        title: "Red jacket for sale",
-        price: 100,
-        image: require("../assets/jacket.jpg"),
-    },
-    {
-        id: 2,
-        title: "Couch in great condition",
-        price: 300,
-        image: require("../assets/couch.jpg"),
-    },
-];
-
 function ListingsScreen({ navigation }) {
+    const [listings, setListings] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        loadListings();
+    }, []);
+
+    const loadListings = async () => {
+        setLoading(true);
+        const response = await listingsApi.getListings();
+        setLoading(false);
+
+        if (!response.ok) {
+            setError(true);
+            return;
+        }
+        setError(false);
+        setListings(response.data);
+    };
+
     return (
         <Screen style={styles.container}>
+            {error && (
+                <View>
+                    <AppText>Couldn't retreave the listings.</AppText>
+                    <AppButton title="Retry" onPress={loadListings}></AppButton>
+                </View>
+            )}
+            <ActivityIndicator visible={loading} />
             <FlatList
                 data={listings}
                 keyExtractor={(item) => item.id.toString()}
@@ -36,7 +53,7 @@ function ListingsScreen({ navigation }) {
                     <Card
                         title={item.title}
                         subTitle={"$" + item.price}
-                        image={item.image}
+                        imageUrl={item.images[0].url}
                         onPress={() => navigation.navigate("ListingDetails", item)}
                     />
                 )}
