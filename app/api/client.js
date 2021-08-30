fcb24c949129dadc5bc826c0c0b7f9bca97d0c97
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-08-30 17:28:08 +0300

    some changes

--- app/api/client.js
+++ app/api/client.js
@@ -1,9 +1,10 @@
 import { create } from "apisauce";
 import cache from "../utility/cache";
 import authStorage from "../auth/storage";
+import settings from "../config/settings";
 
 const apiClient = create({
-    baseURL: "http://192.168.100.7:9000/api",
+    baseURL: settings.apiURL,
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
